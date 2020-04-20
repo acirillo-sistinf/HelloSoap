7a027c2aaa79e5e88ba8d2bf36f9a6220ca2e1aa
Andrea Cirillo <andrea.cirillo@sistinf.it>
2020-04-20 11:05:29 +0200

    First commit

---
+++ src/main/java/it/sistinf/crash/hellosoap/HellosoapApplication.java
@@ -0,0 +1,13 @@
+package it.sistinf.crash.hellosoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/sistinf/crash/hellosoap/HellosoapApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sistinf.crash.hellosoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellosoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
